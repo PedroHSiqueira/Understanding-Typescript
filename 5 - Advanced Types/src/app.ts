7184c3b47a061612ec23c8b0f7e76db34f7ddb7f
Pedro Siqueira <pedrohenriquedoamaralsiqueira@gmail.com>
2023-09-24 17:51:07 -0300

    Index Properties

--- 5 - Advanced Types/src/app.ts
+++ 5 - Advanced Types/src/app.ts
@@ -125,4 +125,15 @@ moveAnimal({type: "horse", runningSpeed: 20})
 // const userInputElement = <HTMLInputElement>document.getElementById("user-input")!
 const userInputElement = document.getElementById("user-input")! as HTMLInputElement
 
-userInputElement.value = "Hi there!"
+if(userInputElement){
+  (userInputElement as HTMLInputElement).value = "Hi there!"
+}
+
+interface ErrorContainer {
+  [prop: string]: string
+}
+
+const errorBag: ErrorContainer = {
+  email: "Not a valid email!",
+  username: "Must start with a capital Letter!"
+}
