d293e923005a2fd15c13fe0af63371b48b83181f
Pedro Siqueira <pedrohenriquedoamaralsiqueira@gmail.com>
2023-09-25 17:48:37 -0300

    Function Overloads

--- 5 - Advanced Types/src/app.ts
+++ 5 - Advanced Types/src/app.ts
@@ -25,13 +25,20 @@ type universal = combinable & numeric
 
 // ------------------------------------------------------------------
 
-function add(a: number | string, b: number | string){
+//Function Overloads
+
+function add(a: number, b: number): number;
+function add(a: string, b: string): string;
+function add(a: combinable, b: combinable){
   if(typeof a === "string" || typeof b === "string"){
     return a.toString() + b.toString()
   }
 
   return a + b
 }
+
+const result = add("Pedro", " Henrique")
+result.split(" ")
 
 // Type Guards 
 
