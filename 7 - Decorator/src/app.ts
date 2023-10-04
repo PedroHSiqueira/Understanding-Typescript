eb254678305c7573df72c2a586e8545fb5fac60f
Pedro Siqueira <pedrohenriquedoamaralsiqueira@gmail.com>
2023-10-03 22:28:31 -0300

    Property Decorators

--- 7 - Decorator/src/app.ts
+++ 7 - Decorator/src/app.ts
@@ -14,7 +14,7 @@ function WithTemplate(template: string, hookId: string){
   }
 }
 
-//Order Bottom to Top
+//Ordem de baixo para cima
 @Logger("Logging")
 @WithTemplate("<h1> My First Object </h1>", "app")
 class Person {
@@ -28,3 +28,36 @@ class Person {
 const pers = new Person()
 
 console.log(pers);
+
+// -----------
+
+function Log(target: any, propertyName: string | symbol){
+console.log("Property Decorator");
+console.log(target, propertyName);
+}
+
+
+class Product{
+  @Log
+  title: string;
+  private _price: number;
+
+  
+  public set price(val: number) {
+    if(val > 0){
+      this._price = val;
+    }else{
+      throw new Error("Invalid Input - Must Be a Positve Number")
+    }
+  }
+  
+
+  constructor(t: string, p: number){
+    this.title = t;
+    this._price = p
+  }
+
+  getPriceWithTax(tax: number){
+    return this._price * (1 + tax)
+  }
+}
