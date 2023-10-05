c2b5a3f96bfd7591d8f464990131aa54ee8c374e
Pedro Siqueira <pedrohenriquedoamaralsiqueira@gmail.com>
2023-10-05 20:28:07 -0300

    Accessor & Parameter Decorators

--- 7 - Decorator/src/app.ts
+++ 7 - Decorator/src/app.ts
@@ -36,6 +36,26 @@ console.log("Property Decorator");
 console.log(target, propertyName);
 }
 
+function Log2(target: any, name: string, descriptor: PropertyDescriptor){
+  console.log("Access Decorator");
+  console.log(target);
+  console.log(name);
+  console.log(descriptor);
+}
+
+function Log3(target: any, name: string | symbol, descriptor: PropertyDescriptor){
+  console.log("Method Decorator");
+  console.log(target);
+  console.log(name);
+  console.log(descriptor);
+}
+
+function Log4(target: any, name: string | symbol, position: number){
+  console.log("Parameter Decorator");
+  console.log(target);
+  console.log(name);
+  console.log(position);
+}
 
 class Product{
   @Log
@@ -43,6 +63,7 @@ class Product{
   private _price: number;
 
   
+  @Log2
   public set price(val: number) {
     if(val > 0){
       this._price = val;
@@ -57,7 +78,8 @@ class Product{
     this._price = p
   }
 
-  getPriceWithTax(tax: number){
+  @Log3
+  getPriceWithTax(@Log4 tax: number){
     return this._price * (1 + tax)
   }
 }
