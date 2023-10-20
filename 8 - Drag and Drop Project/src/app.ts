95ba9da958d1bc4a8557559b5a07f24ddb042f40
Pedro Siqueira <pedrohenriquedoamaralsiqueira@gmail.com>
2023-10-20 20:37:54 -0300

    Finishing the Project

--- 8 - Drag and Drop Project/src/app.ts
+++ 8 - Drag and Drop Project/src/app.ts
@@ -65,6 +65,18 @@ class ProjectState extends State<Project> {
       ProjectStatus.active
     );
     this.projects.push(newProjects);
+    this.updateListeners()
+  }
+
+  moveProject(projectId: string, newStatus: ProjectStatus){
+    const project = this.projects.find(prj => prj.id === projectId)
+    if(project && project.status !== newStatus){
+      project.status = newStatus;
+      this.updateListeners()
+    }
+  }
+
+  private updateListeners(){
     for (const listenerFn of this.listeners) {
       listenerFn(this.projects.slice());
     }
@@ -228,7 +240,7 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement> implements Drag
   }
 
   @autoBind
-  dragOverHandler(event: DragEvent): void {
+  public dragOverHandler(event: DragEvent): void {
     if(event.dataTransfer && event.dataTransfer.types[0] === "text/plain"){
       event.preventDefault()
       const listEl = this.element.querySelector("ul")!;
@@ -236,12 +248,14 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement> implements Drag
     }
   }
   
-  dropHandler(event: DragEvent): void {
-    console.log(event.dataTransfer!.getData("text/plain"));
+  @autoBind
+  public dropHandler(event: DragEvent): void {
+    const prjId = event.dataTransfer!.getData("text/plain")
+    projectState.moveProject(prjId, this.type === "active" ? ProjectStatus.active : ProjectStatus.finished )
   }
   
   @autoBind
-  dragLeaveHandler(_: DragEvent): void {
+  public dragLeaveHandler(_: DragEvent): void {
     const listEl = this.element.querySelector("ul")!;
     listEl.classList.remove("droppable")
   }
