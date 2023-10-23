7b9a56974fe897c29f460fc10b44ee3ff030a947
Pedro Siqueira <pedrohenriquedoamaralsiqueira@gmail.com>
2023-10-23 17:31:34 -0300

    Split in multiple files using Namespaces

---
+++ 8 - Drag and Drop Project/src/state/project-state.ts
@@ -0,0 +1,59 @@
+namespace App{
+  //Project State Management
+
+  type Listener<T> = (items: T[]) => void;
+
+  class State<T> {
+    protected listeners: Listener<T>[] = [];
+
+    public addListener(listenerFn: Listener<T>) {
+      this.listeners.push(listenerFn);
+    }
+  }
+
+  export class ProjectState extends State<Project> {
+    private projects: Project[] = [];
+    private static instance: ProjectState;
+
+    private constructor() {
+      super();
+    }
+
+    static getInstance() {
+      if (this.instance) {
+        return this.instance;
+      } else {
+        this.instance = new ProjectState();
+        return this.instance;
+      }
+    }
+
+    addProject(title: string, description: string, numOfPeople: number) {
+      const newProjects = new Project(
+        Math.random().toString(),
+        title,
+        description,
+        numOfPeople,
+        ProjectStatus.active
+      );
+      this.projects.push(newProjects);
+      this.updateListeners();
+    }
+
+    moveProject(projectId: string, newStatus: ProjectStatus) {
+      const project = this.projects.find((prj) => prj.id === projectId);
+      if (project && project.status !== newStatus) {
+        project.status = newStatus;
+        this.updateListeners();
+      }
+    }
+
+    private updateListeners() {
+      for (const listenerFn of this.listeners) {
+        listenerFn(this.projects.slice());
+      }
+    }
+  }
+
+  export const projectState = ProjectState.getInstance();
+}
