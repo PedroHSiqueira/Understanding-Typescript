2d7f3f7307ef74b650df3efd25bcd9c12cd77319
Pedro Siqueira <pedrohenriquedoamaralsiqueira@gmail.com>
2023-10-31 18:42:53 -0300

    Change folders and Next Section

---
+++ 9 - React and NodeJs/NodeJS and Express Module/src/app.ts
@@ -0,0 +1,13 @@
+import express, {Request, Response, NextFunction} from "express"
+
+import TodoRoutes from "./routes/todos"
+
+const app = express()
+
+app.use("/todos", TodoRoutes)
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  res.status(500).json({message: err.message})
+})
+
+app.listen(3000)
